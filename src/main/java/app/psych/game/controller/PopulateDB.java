32ecba013f39ec28525dc5c6d430afa40e625935
Mohit Musaddi <96mohitm@gmail.com>
2019-12-31 23:08:05 +0530

    Add image url in dummy players

--- src/main/java/app/psych/game/controller/PopulateDB.java
+++ src/main/java/app/psych/game/controller/PopulateDB.java
@@ -1,5 +1,13 @@
 package app.psych.game.controller;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import app.psych.game.Constants;
 import app.psych.game.Pair;
 import app.psych.game.Utils;
@@ -8,13 +16,6 @@ import app.psych.game.model.Player;
 import app.psych.game.model.Question;
 import app.psych.game.repository.PlayerRepository;
 import app.psych.game.repository.QuestionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/dev")
@@ -50,8 +51,12 @@ public class PopulateDB {
         playerRepository.deleteAll();
         Player luffy = new Player();
         luffy.setName("Monkey D. Luffy");
+        luffy.setPicURL("https://i.imgur.com/PrCEBd7.png");
+        luffy.setPsychFaceURL("https://i.imgur.com/SPzynwl.png");
         Player robin = new Player();
         robin.setName("Nico Robin");
+        robin.setPicURL("https://i.imgur.com/kB7StJm.png");
+        robin.setPicURL("https://i.imgur.com/tnJTeaG.png");
 
         playerRepository.save(luffy);
         playerRepository.save(robin);
